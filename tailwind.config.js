154ccaa8e16a7fce1486a180e22b723d036a20c2
Fitri Ratna Dewi <fratnadewi@gmail.com>
2023-01-10 11:39:05 +0700

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        first: "#F8EDE3",
+        second: "#DFD3C3",
+        third: "#D0B8A8",
+        fourth: "#85586F",
+      },
+    },
   },
   plugins: [],
 };
