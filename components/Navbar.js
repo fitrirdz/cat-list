62918495c9539b603a4a402a00bf05bd0b71c27d
Fitri Ratna Dewi <fratnadewi@gmail.com>
2023-01-10 16:05:37 +0700

    Use icon and logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,13 +4,15 @@ import SearchField from "./SearchField";
 const Navbar = ({ keyword, setKeyword }) => {
   return (
     <div className="w-full h-28 p-4 fixed top-0 left-0 right-0 bg-fourth shadow-md text-white flex flex-row justify-between items-center space-x-16">
-      <div className="shrink-0">Logo here</div>
+      <div className="shrink-0">
+        <img src="./cat-logo.webp" height={100} width={100} />
+      </div>
       <div className="w-full">
         <SearchField keyword={keyword} setKeyword={setKeyword} />
       </div>
       <div className="font-bold text-right text-xl space-x-6 shrink-0">
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        {/* <a href="/about">About</a>
+        <a href="/contact">Contact</a> */}
       </div>
     </div>
   );
